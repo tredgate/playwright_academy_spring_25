ed8d5d036424a04badeb082f7686ed86bae6a77e
Petr Fifka <petrfifk@gmail.com>
2025-05-22 20:02:37 +0200

    set github actions tags

--- tests/prep/visual/visual_tests.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.describe("Visual Tests", () => {
-  test("Simple Visual Test", async ({ page }) => {
-    await page.goto("https://tredgate.com/webtrain/web-actions.html");
-    await expect(page).toHaveScreenshot("simple_test.png");
-  });
-});
